0ec08404fe598c2313f7353b3047a32be3960dd6
Margarita <Margarita@DATA_SERVER>
2021-11-26 17:13:18 +0400

    Add project files.

---
+++ _Dictionary/Program.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+            List<int> keyList = new List<int>(myDictionary.Count);
+
+            for (int i = 0; i < 99; i++)
+            {
+                a += "a";
+                myDictionary.Add(i, a);
+                Console.WriteLine($"Key: {i},  Value: {a}");
+                keyList.Add(i);
+            }
+            
+            Console.WriteLine($"Dictionaray elements count: {myDictionary.Count}");
+
+            myDictionary[0] = "b";
+            myDictionary[99] = "f";
+            Console.WriteLine($"{myDictionary[0]}{myDictionary[99]}");
+
+            for (int i = 0; i < myDictionary.Count; i++)
+            {                
+                Console.WriteLine($"Key: {i},  Value: {myDictionary[i]}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Dictionary<TKey, TValue>
+    {
+        private struct Entry
+        {
+            public int hashCode;    // Lower 31 bits of hash code, -1 if unused
+            public int next;        // Index of next entry, -1 if last
+            public TKey key;           // Key of entry
+            public TValue value;         // Value of entry
+        }
+
+        private int[] buckets;
+        private Entry[] entries;
+        private int count;
+        private int version;
+        private int freeList;
+        private int freeCount;
+        private IEqualityComparer<TKey> comparer;
+
+        // constants for serialization
+        private const String VersionName = "Version";
+        private const String HashSizeName = "HashSize";  // Must save buckets.Length
+        private const String KeyValuePairsName = "KeyValuePairs";
+        private const String ComparerName = "Comparer";
+
+        public Dictionary() : this(0, null) { }
+
+        public Dictionary(int capacity) : this(capacity, null) { }
+
+        public Dictionary(int capacity, IEqualityComparer<TKey> comparer)
+        {
+            if (capacity < 0) throw new ArgumentOutOfRangeException();
+            if (capacity > 0) Initialize(capacity);
+            this.comparer = comparer ?? EqualityComparer<TKey>.Default;
+        }
+
+        public IEqualityComparer<TKey> Comparer
+        {
+            get
+            {
+                return comparer;
+            }
+        }
+
+        public int Count
+        {
+            get { return count - freeCount; }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int i = FindEntry(key);
+                if (i >= 0) return entries[i].value;
+                throw new KeyNotFoundException();
+                return default(TValue);
+            }
+            set
+            {
+                Insert(key, value, false);
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Insert(key, value, true);
+        }
+
+        public void Clear()
+        {
+            if (count > 0)
+            {
+                for (int i = 0; i < buckets.Length; i++) buckets[i] = -1;
+                Array.Clear(entries, 0, count);
+                freeList = -1;
+                count = 0;
+                freeCount = 0;
+                version++;
+            }
+        }
+
+        private int FindEntry(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (buckets != null)
+            {
+                int hashCode = comparer.GetHashCode(key) & 0x7FFFFFFF;
+                for (int i = buckets[hashCode % buckets.Length]; i >= 0; i = entries[i].next)
+                {
+                    if (entries[i].hashCode == hashCode && comparer.Equals(entries[i].key, key)) return i;
+                }
+            }
+            return -1;
+        }
+
+        private void Initialize(int capacity)
+        {
+            int size = HashHelpers.GetPrime(capacity);
+            buckets = new int[size];
+            for (int i = 0; i < buckets.Length; i++) buckets[i] = -1;
+            entries = new Entry[size];
+            freeList = -1;
+        }
+
+        private void Insert(TKey key, TValue value, bool add)
+        {
+
+            if (key == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (buckets == null) Initialize(0);
+            int hashCode = comparer.GetHashCode(key) & 0x7FFFFFFF;
+            int targetBucket = hashCode % buckets.Length;
+
+            int collisionCount = 0;
+
+            for (int i = buckets[targetBucket]; i >= 0; i = entries[i].next)
+            {
+                if (entries[i].hashCode == hashCode && comparer.Equals(entries[i].key, key))
+                {
+                    if (add)
+                    {
+                        throw new ArgumentException();
+                    }
+                    entries[i].value = value;
+                    version++;
+                    return;
+                }
+
+                collisionCount++;
+            }
+            int index;
+            if (freeCount > 0)
+            {
+                index = freeList;
+                freeList = entries[index].next;
+                freeCount--;
+            }
+            else
+            {
+                if (count == entries.Length)
+                {
+                    Resize();
+                    targetBucket = hashCode % buckets.Length;
+                }
+                index = count;
+                count++;
+            }
+
+            entries[index].hashCode = hashCode;
+            entries[index].next = buckets[targetBucket];
+            entries[index].key = key;
+            entries[index].value = value;
+            buckets[targetBucket] = index;
+            version++;
+        }
+
+        private void Resize()
+        {
+            Resize(HashHelpers.ExpandPrime(count), false);
+        }
+
+        private void Resize(int newSize, bool forceNewHashCodes)
+        {
+            int[] newBuckets = new int[newSize];
+            for (int i = 0; i < newBuckets.Length; i++) newBuckets[i] = -1;
+            Entry[] newEntries = new Entry[newSize];
+            Array.Copy(entries, 0, newEntries, 0, count);
+            if (forceNewHashCodes)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (newEntries[i].hashCode != -1)
+                    {
+                        newEntries[i].hashCode = (comparer.GetHashCode(newEntries[i].key) & 0x7FFFFFFF);
+                    }
+                }
+            }
+            for (int i = 0; i < count; i++)
+            {
+                if (newEntries[i].hashCode >= 0)
+                {
+                    int bucket = newEntries[i].hashCode % newSize;
+                    newEntries[i].next = newBuckets[bucket];
+                    newBuckets[bucket] = i;
+                }
+            }
+            buckets = newBuckets;
+            entries = newEntries;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int i = FindEntry(key);
+            if (i >= 0)
+            {
+                value = entries[i].value;
+                return true;
+            }
+            value = default(TValue);
+            return false;
+        }
+
+        internal static class HashHelpers
+        {
+            public const int MaxPrimeArrayLength = 0x7FEFFFFD;
+            internal const Int32 HashPrime = 101;
+            public static readonly int[] primes = {
+            3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
+            1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+            17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
+            187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+            1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369};
+
+            public static bool IsPrime(int candidate)
+            {
+                if ((candidate & 1) != 0)
+                {
+                    int limit = (int)Math.Sqrt(candidate);
+                    for (int divisor = 3; divisor <= limit; divisor += 2)
+                    {
+                        if ((candidate % divisor) == 0)
+                            return false;
+                    }
+                    return true;
+                }
+                return (candidate == 2);
+            }
+
+            public static int GetPrime(int min)
+            {
+                if (min < 0)
+                    throw new ArgumentException();
+
+                for (int i = 0; i < primes.Length; i++)
+                {
+                    int prime = primes[i];
+                    if (prime >= min) return prime;
+                }
+
+                for (int i = (min | 1); i < Int32.MaxValue; i += 2)
+                {
+                    if (IsPrime(i) && ((i - 1) % HashHelpers.HashPrime != 0))
+                        return i;
+                }
+                return min;
+            }
+
+            public static int GetMinPrime()
+            {
+                return primes[0];
+            }
+
+            public static int ExpandPrime(int oldSize)
+            {
+                int newSize = 2 * oldSize;
+
+                if ((uint)newSize > MaxPrimeArrayLength && MaxPrimeArrayLength > oldSize)
+                {
+                    return MaxPrimeArrayLength;
+                }
+
+                return GetPrime(newSize);
+            }
+        }
+    }
+}
